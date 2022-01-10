0dc29d51881deac177374eea453b2af195182d7f
hjyk0310 <kjyk0835@naver.com>
2022-01-11 06:39:49 +0900

    modified fragment_partner_search.xml, grid-recyclerView 구현, colors.xml의 일부 color name 수정 todo: 1. grid-recyclerView decoration써서 상하, 좌우 margin 주기 2. 추천 파트너 프로필이미지 화면 width에 맞춰 동적으로 크기조절되게 수정 3. scrollView 어떻게할지 생각

--- app/src/main/java/com/example/duos/ui/main/partnerSearch/PartnerSearchFragment.kt
+++ app/src/main/java/com/example/duos/ui/main/partnerSearch/PartnerSearchFragment.kt
@@ -1,12 +1,41 @@
 package com.example.duos.ui.main.partnerSearch
 
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.duos.R
+import com.example.duos.data.entities.RecommendedPartner
 import com.example.duos.databinding.FragmentPartnerSearchBinding
 import com.example.duos.ui.BaseFragment
 
 class PartnerSearchFragment(): BaseFragment<FragmentPartnerSearchBinding>(FragmentPartnerSearchBinding::inflate) {
+    private var recommendedPartnerDatas = ArrayList<RecommendedPartner>()
 
     override fun initAfterBinding() {
+        recommendedPartnerDatas.apply {
+            // 서울시 마포구로 필터링됐을 떄를 가정
+            // FLO clone coding 211110자 커밋 참고했음
+            add(RecommendedPartner(R.drawable.partner_profile_img_1, "서울시 마포구", "구력 1년", "berlinalena", "30대", 4.8, 11))
+            add(RecommendedPartner(R.drawable.partner_profile_img_2, "서울시 서대문구", "구력 3년", "time456 ","30대", 4.7, 21))
+            add(RecommendedPartner(R.drawable.partner_profile_img_3, "서울시 마포구", "구력 2년", "qop123", "20대", 4.7, 15))
+            add(RecommendedPartner(R.drawable.partner_profile_img_4, "서울시 마포구", "구력 1년", "Olivia", "30대", 4.6, 8))
+            add(RecommendedPartner(R.drawable.partner_profile_img_2, "서울시 마포구", "구력 1년", "berlinalena", "30대", 4.8, 11))
+            add(RecommendedPartner(R.drawable.partner_profile_img_1, "서울시 서대문구", "구력 3년", "time456 ","30대", 4.7, 21))
+            add(RecommendedPartner(R.drawable.partner_profile_img_4, "서울시 마포구", "구력 2년", "qop123", "20대", 4.7, 15))
+            add(RecommendedPartner(R.drawable.partner_profile_img_3, "서울시 마포구", "구력 1년", "Olivia", "30대", 4.6, 8))
+            add(RecommendedPartner(R.drawable.partner_profile_img_1, "서울시 마포구", "구력 1년", "berlinalena", "30대", 4.8, 11))
+            add(RecommendedPartner(R.drawable.partner_profile_img_2, "서울시 마포구", "구력 3년", "time456 ","30대", 4.7, 21))
+        }
 
+        binding.partnerSearchRecommendedPartnerRv.layoutManager = GridLayoutManager(context, 2)
+
+        val partnerSearchRVGridAdapter = PartnerSearchRVGridAdapter(recommendedPartnerDatas)
+
+        binding.partnerSearchRecommendedPartnerRv.adapter = partnerSearchRVGridAdapter
+
+        partnerSearchRVGridAdapter.setRecommendedPartnerItemClickListener(object:PartnerSearchRVGridAdapter.recommendedPartnerItemClickListener{
+            override fun onItemClick(recommendedPartner: RecommendedPartner) {
+                // 파트너 세부 화면으로 이동
+            }
+        })
     }
 
     companion object {
---
+++ app/src/main/java/com/example/duos/ui/main/partnerSearch/PartnerSearchRVGridAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.duos.ui.main.partnerSearch
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.duos.data.entities.RecommendedPartner
+import com.example.duos.databinding.RecommendedPartnerGridItemBinding
+
+class PartnerSearchRVGridAdapter(private val recommendedPartnerList: ArrayList<RecommendedPartner>): RecyclerView.Adapter<PartnerSearchRVGridAdapter.ViewHolder>() {
+    //private val recommendedPartners = ArrayList<RecommendedPartner>()
+
+    // 클릭 인터페이스 정의
+    interface recommendedPartnerItemClickListener{
+        fun onItemClick(recommendedPartner: RecommendedPartner)
+    }
+
+    // 리스너 객체를 전달받는 함수랑 리스너 객체를 저장할 변수
+    private lateinit var mItemClickListener: recommendedPartnerItemClickListener
+
+    fun setRecommendedPartnerItemClickListener(itemClickListener: recommendedPartnerItemClickListener){
+        mItemClickListener = itemClickListener
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val binding: RecommendedPartnerGridItemBinding = RecommendedPartnerGridItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(recommendedPartnerList[position])
+    }
+
+    override fun getItemCount(): Int = recommendedPartnerList.size
+
+//    fun addRecommendedPartners(recommendedPartners: ArrayList<RecommendedPartner>){
+//        this.recommendedPartners.clear()
+//        this.recommendedPartners.addAll(recommendedPartners)
+//
+//        notifyDataSetChanged()
+//    }
+
+    inner class ViewHolder(val binding: RecommendedPartnerGridItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(recommendedPartner : RecommendedPartner){
+            binding.partnerSearchRecommendedPartnerIv.setImageResource(recommendedPartner.profileImg!!)
+            binding.partnerSearchRecommendedPartnerLocationTv.text = recommendedPartner.location
+            binding.partnerSearchRecommendedPartnerBallCapabilityTv.text = recommendedPartner.ballCapacity
+            binding.partnerSearchRecommendedPartnerIdTv.text = recommendedPartner.id
+            binding.partnerSearchRecommendedPartnerAgeTv.text = recommendedPartner.age
+            binding.partnerSearchRecommendedPartnerStarRatingTv.text = recommendedPartner.starRating.toString()
+            binding.partnerSearchRecommendedPartnerReviewCountTv.text = recommendedPartner.reviewCount.toString()
+        }
+    }
+}
