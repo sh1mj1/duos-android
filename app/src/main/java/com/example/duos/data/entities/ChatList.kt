fc9d927a5901b625d06d5c9f155a45528570dbc0
hjyk0310 <kjyk0835@naver.com>
2022-01-14 22:07:54 +0900

    채팅 목록 리사이클러뷰 아이템 xml 수정, bottomNavigation 친구목록과 채팅 프래그먼트가 반대로 연결되어있어서 수정, ChatFragment를 ChatListFragment로 수정

---
+++ app/src/main/java/com/example/duos/data/entities/ChatList.kt
@@ -0,0 +1,8 @@
+package com.example.duos.data.entities
+
+data class ChatList(
+    var profileImg : String = "",
+    var id : String = "",
+    var contentPreview : String = "",
+    var receivedTime : String = ""
+)
--- app/src/main/java/com/example/duos/ui/main/chat/ChatListFragment.kt
+++ app/src/main/java/com/example/duos/ui/main/chat/ChatListFragment.kt
@@ -3,13 +3,13 @@ package com.example.duos.ui.main.chat
 import com.example.duos.databinding.FragmentChatBinding
 import com.example.duos.ui.BaseFragment
 
-class ChatFragment(): BaseFragment<FragmentChatBinding>(FragmentChatBinding::inflate) {
+class ChatListFragment(): BaseFragment<FragmentChatBinding>(FragmentChatBinding::inflate) {
 
     override fun initAfterBinding() {
 
     }
 
     companion object {
-        fun newInstance(): ChatFragment = ChatFragment()
+        fun newInstance(): ChatListFragment = ChatListFragment()
     }
 }
