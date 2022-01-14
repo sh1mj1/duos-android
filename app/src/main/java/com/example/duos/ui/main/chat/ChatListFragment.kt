2d3fa12c2c074b495ad331236d8bffae7cff8f19
hjyk0310 <kjyk0835@naver.com>
2022-01-15 03:31:09 +0900

    ui 변경된 사항 모두 수정, 아이콘 png 파일들 화질 깨져서 svg로 다운받아 vector asset으로 변경, 채팅 목록 리사이클러뷰 height 문제 해결, 상태바 일단 primary 색으로 변경

--- app/src/main/java/com/example/duos/ui/main/chat/ChatListFragment.kt
+++ app/src/main/java/com/example/duos/ui/main/chat/ChatListFragment.kt
@@ -20,6 +20,14 @@ class ChatListFragment(): BaseFragment<FragmentChatListBinding>(FragmentChatList
             add(ChatList(R.drawable.chat_profile_img_2, "uiii_88", "알겠습니다", "오전 09:11"))
             add(ChatList(R.drawable.chat_profile_img_3, "djeikd0620", "넵~", "오후 11:33"))
             add(ChatList(R.drawable.chat_profile_img_4, "drahm0422", "아~~ 그러면", "오후 04:14"))
+            add(ChatList(R.drawable.chat_profile_img_1, "4evertennis", "네네 그럼 그때 뵐게요~!!", "오후 10:59"))
+            add(ChatList(R.drawable.chat_profile_img_2, "uiii_88", "알겠습니다", "오전 09:11"))
+            add(ChatList(R.drawable.chat_profile_img_3, "djeikd0620", "넵~", "오후 11:33"))
+            add(ChatList(R.drawable.chat_profile_img_4, "drahm0422", "아~~ 그러면", "오후 04:14"))
+            add(ChatList(R.drawable.chat_profile_img_1, "4evertennis", "네네 그럼 그때 뵐게요~!!", "오후 10:59"))
+            add(ChatList(R.drawable.chat_profile_img_2, "uiii_88", "알겠습니다", "오전 09:11"))
+            add(ChatList(R.drawable.chat_profile_img_3, "djeikd0620", "넵~", "오후 11:33"))
+            add(ChatList(R.drawable.chat_profile_img_4, "drahm0422", "아~~ 그러면", "오후 04:14"))
         }
 
         val chatListRVAdapter = ChatListRVAdapter(chatListDatas)
