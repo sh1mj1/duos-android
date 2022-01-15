dcd8a60a8b78fa8a728630cb70f2c5d7a804ddb1
jyeon <cute060500@naver.com>
2022-01-15 18:55:54 +0900

    마이페이지 홈 xml 완성 / 프로필 누르면 프로필 편집 activity 로 넘어감

--- app/src/main/java/com/example/duos/data/entities/User.kt
+++ app/src/main/java/com/example/duos/data/entities/User.kt
@@ -5,15 +5,11 @@ import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 // 사용자 정보 데이터 클래스
-@Entity(tableName = "UserTable")
 data class User(
-    @SerializedName("phone") val phone: String,
-    @SerializedName("password") val password: String,
-    @SerializedName("name") val name: String,
-    @SerializedName("sex") val sex: String
+    val phone: String,
+    val password: String,
+    val nickname: String,
+    val sex: String
     // 지역, 구력, 프로필 사진 등 추가
-){
-    @PrimaryKey(autoGenerate = true)
-    var id: Int = 0
-}
+)
 
