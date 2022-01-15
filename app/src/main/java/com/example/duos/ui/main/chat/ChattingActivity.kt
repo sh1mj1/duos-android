90bd7c1e8ce67b5a0654f2b432d544101d6d2047
hjyk0310 <kjyk0835@naver.com>
2022-01-16 04:15:42 +0900

    채팅화면 xml

---
+++ app/src/main/java/com/example/duos/ui/main/chat/ChattingActivity.kt
@@ -0,0 +1,10 @@
+package com.example.duos.ui.main.chat
+
+import com.example.duos.databinding.ActivityChattingBinding
+import com.example.duos.ui.BaseActivity
+
+class ChattingActivity: BaseActivity<ActivityChattingBinding>(ActivityChattingBinding::inflate) {
+    override fun initAfterBinding() {
+        
+    }
+}
