410c2275f14821ef14244844c9fa704bd29457a7
sh1mj1 <7wlgns@gmail.com>
2022-01-25 18:34:11 +0900

    사진 업로드 기능 회원가입에서 완

--- app/src/main/java/com/example/duos/ui/signup/SignUpFragment05.kt
+++ app/src/main/java/com/example/duos/ui/signup/SignUpFragment05.kt
@@ -1,7 +1,6 @@
 package com.example.duos.ui.signup
 
 import android.Manifest
-import android.app.Activity.BLOB_STORE_SERVICE
 import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
 import android.content.DialogInterface
@@ -9,6 +8,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
 import android.media.ExifInterface
 import android.net.Uri
 import android.os.Build
@@ -17,14 +17,12 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.example.duos.R
 import com.example.duos.databinding.FragmentSignup05Binding
 import com.example.duos.ui.BaseFragment
 import java.io.File
-import android.content.ContentResolver
 import android.graphics.Matrix
 
 
@@ -35,13 +33,20 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
     val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
     val CAMERA_PERMISSION_REQUEST = 100
 
-    // 멀티 퍼미션
-    val requiredPermissions = arrayOf(
+    // 멀티 퍼미션 카메라1
+    val requiredPermissions1 = arrayOf(
         Manifest.permission.READ_EXTERNAL_STORAGE,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.CAMERA,
     )
-    val multiplePermissionsCode = 200
+    val multiplePermissionsCode1 = 200
+
+    // 멀티퍼미션 갤러리(앨범)2
+    val permission_list = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.ACCESS_MEDIA_LOCATION
+    )
+    val multiplePermissionsCode2 = 300
 
 
     override fun initAfterBinding() {
@@ -88,7 +93,7 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
                             // 거부 Or 아직 수락하지 않은 퍼미션을 저장할 String 배열리스트
                             val rejectedPermissionList = ArrayList<String>()
                             // 필요한 퍼미션들이 현재 권한을 받았는지 체크
-                            for (permission in requiredPermissions) {
+                            for (permission in requiredPermissions1) {
                                 if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
                                     //만약 권한이 없다면 rejectedPermissionList에 추가
                                     rejectedPermissionList.add(permission)
@@ -97,7 +102,7 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
 
                             if (rejectedPermissionList.isNotEmpty()) {   // 거절된 퍼미션 있다면?
                                 val array = arrayOfNulls<String>(rejectedPermissionList.size)
-                                requestPermissions(rejectedPermissionList.toArray(array), multiplePermissionsCode)
+                                requestPermissions(rejectedPermissionList.toArray(array), multiplePermissionsCode1)
                             } else {// 모두 허용된 퍼미션이라면
                                 Log.d("Signup_Image_Upload_Dialog", "PERMISSION_GRANTED$which")
                                 val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -114,11 +119,35 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
                                 contentUri = FileProvider.getUriForFile(requireContext(), "com.duos.camera.file_provider", file)
 
                                 intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
-                                startActivityForResult(intent, multiplePermissionsCode)
+                                startActivityForResult(intent, multiplePermissionsCode1)
                             }
                         }
                         // 파일 접근
-                        2 -> Log.d("Signup_Image_Upload_Dialog", "파일 접근 $which")
+                        2 -> {
+                            Log.d("Signup_Image_Upload_Dialog", "파일 접근 $which")
+                            val rejectedPermissionList = ArrayList<String>()
+                            // 필요한 퍼미션들이 현재 권한을 받았는지 체크
+                            for (permission in permission_list) {
+                                if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                                    //만약 권한이 없다면 rejectedPermissionList에 추가
+                                    rejectedPermissionList.add(permission)
+                                }
+                            }
+                            if (rejectedPermissionList.isNotEmpty()) {   // 거절된 퍼미션 있다면?
+                                val array = arrayOfNulls<String>(rejectedPermissionList.size)
+                                requestPermissions(rejectedPermissionList.toArray(array), multiplePermissionsCode2)
+                            } else {
+
+                                // 앨범에서 사진을 선택할 수 있도록 Intent
+                                val albumIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                                // 실행할 액티비티의 타입 설정(이미지를 선택할 수 있는 것)
+                                albumIntent.type = "image/*"
+                                // 선택할 파일의 타입 지정
+                                val mimeType = arrayOf("image/*")
+                                albumIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeType)
+                                startActivityForResult(albumIntent, multiplePermissionsCode2)
+                            }
+                        }
                     }
                 })
             dialogBuilder.create().show()
@@ -150,7 +179,7 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
                 }
             }
 
-            multiplePermissionsCode -> {
+            multiplePermissionsCode1 -> {
                 var startCam = true
                 if (grantResults.isNotEmpty()) {
                     for ((i, permission) in permissions.withIndex()) {
@@ -178,8 +207,29 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
                         intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
                         startActivityForResult(intent, 200)
                     }
-
-
+                }
+            }
+            multiplePermissionsCode2 -> {
+                var startAlb = true
+                if (grantResults.isNotEmpty()) {
+                    for ((i, permission) in permissions.withIndex()) {
+                        // 권한이 없는 permission이 있다면
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            Log.d("Signup", "사용하려면 권한 체크 해야되")
+                            Toast.makeText(requireContext(), "프로필 사진을 업로드하려면 카메라 접근 권한을 허용해야 합니다.", Toast.LENGTH_LONG).show()
+                            startAlb = false
+                        }
+                    }
+                    if (startAlb) {
+                        // 앨범에서 사진을 선택할 수 있도록 Intent
+                        val albumIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        // 실행할 액티비티의 타입 설정(이미지를 선택할 수 있는 것)
+                        albumIntent.type = "image/*"
+                        // 선택할 파일의 타입 지정
+                        val mimeType = arrayOf("image/*")
+                        albumIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeType)
+                        startActivityForResult(albumIntent, multiplePermissionsCode2)
+                    }
                 }
             }
         }
@@ -201,14 +251,14 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
                 }
             }
 
-            multiplePermissionsCode -> {
+            multiplePermissionsCode1 -> {
                 if (resultCode == RESULT_OK) {
                     val bitmap = BitmapFactory.decodeFile(contentUri.path)
                     // 사진 조정 된것
                     val degree = getDegree(contentUri, contentUri.path!!)   // contentUri 는 안드로이드 10버전 이상, contentUri.path!! 는 9버전 이하를 위해 넣음
                     val bitmap2 = resizeBitmap(1024, bitmap)
                     val bitmap3 = rotateBitmap(bitmap2, degree)
-                    
+
                     binding.signup05ProfileImageBackgroundIv.setImageBitmap(bitmap3)
 
 
@@ -218,6 +268,36 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
 
                 }
             }
+            multiplePermissionsCode2 -> {
+                if(resultCode == RESULT_OK){
+                    // 선택한 이미지의 경로 데이터를 관리하는 Uri 객체를 추출
+
+                    val uri = data?.data
+
+                    if(uri != null){
+                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                            // 안드로이드 10버전 이상
+                            val source = ImageDecoder.createSource(requireActivity().contentResolver, uri)
+                            val bitmap = ImageDecoder.decodeBitmap(source)
+                            binding.signup05ProfileImageBackgroundIv.setImageBitmap(bitmap)
+                        }else{
+                            val cursor = requireActivity().contentResolver.query(uri, null, null, null, null)
+                            if(cursor != null){
+                                cursor.moveToNext()
+                                // 이미지 경로를 가져온다.
+                                val index = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+                                val source = cursor.getString(index)
+                                // 이미지 생성
+                                val bitmap = BitmapFactory.decodeFile(source)
+                                binding.signup05ProfileImageBackgroundIv.setImageBitmap(bitmap)
+                            }
+                        }
+                    }
+                }
+
+
+            }
+
         }
 
     }
@@ -234,27 +314,28 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
 
     }
     // 이미지의 회전 각도값을 구하기
-//    11버전 이상부터 달라짐 (외부저장소 보안 때문에)
-    fun getDegree(uri:Uri, source: String):Float{
-        var exif:ExifInterface? = null
+    // 11버전 이상부터 달라짐 (외부저장소 보안 때문에)
+    fun getDegree(uri: Uri, source: String): Float {
+        var exif: ExifInterface? = null
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             val photoUri = MediaStore.setRequireOriginal(uri)
             val stream = requireActivity().contentResolver.openInputStream(photoUri)
             exif = ExifInterface(source)
-        }else{
+        } else {
             exif = ExifInterface(source)
         }
         var degree = 0
         var ori = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1)   // 만약 회전값이 저장이 안되어 있으면 default값으로 -1 넣기 (0 넣으면 안댐)
-        when(ori){
+        when (ori) {
             ExifInterface.ORIENTATION_ROTATE_90 -> degree = 90
             ExifInterface.ORIENTATION_ROTATE_180 -> degree = 180
             ExifInterface.ORIENTATION_ROTATE_270 -> degree = 270
         }
         return degree.toFloat()
     }
-    fun rotateBitmap(bitmap:Bitmap, degree : Float) : Bitmap{
+    // 사진 돌리기
+    fun rotateBitmap(bitmap: Bitmap, degree: Float): Bitmap {
 
         // 각도값을 관리하는 객체
         val matrix = Matrix()
@@ -263,7 +344,6 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
         val bitmap2 = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
         return bitmap2
     }
-
 
 
 }
