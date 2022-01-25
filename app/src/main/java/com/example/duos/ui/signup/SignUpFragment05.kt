f540fb1c205cdb6da9d76e4ace7e4208161c816e
sh1mj1 <7wlgns@gmail.com>
2022-01-25 17:17:56 +0900

    갤러리에 저장과 가져오기

--- app/src/main/java/com/example/duos/ui/signup/SignUpFragment05.kt
+++ app/src/main/java/com/example/duos/ui/signup/SignUpFragment05.kt
@@ -9,9 +9,12 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.ExifInterface
 import android.net.Uri
+import android.os.Build
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -21,6 +24,9 @@ import com.example.duos.R
 import com.example.duos.databinding.FragmentSignup05Binding
 import com.example.duos.ui.BaseFragment
 import java.io.File
+import android.content.ContentResolver
+import android.graphics.Matrix
+
 
 class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup05Binding::inflate) {
 
@@ -29,9 +35,13 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
     val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
     val CAMERA_PERMISSION_REQUEST = 100
 
-    val STORAGE_PERMISSION =
-        arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    val STORAGE_PERMISSION_REQUEST = 200
+    // 멀티 퍼미션
+    val requiredPermissions = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.CAMERA,
+    )
+    val multiplePermissionsCode = 200
 
 
     override fun initAfterBinding() {
@@ -47,7 +57,7 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
                 // 이렇게 하면 동적 데이터가 있는 목록(예: 데이터베이스에서 가져온 것을 ListAdapter로 지원할 수 있다.)
                 .setItems(R.array.upload_pic_dialog_title, DialogInterface.OnClickListener { dialog, which ->
                     when (which) {
-                        // 카메라 0
+                        // 카메라 0 썸네일로
                         0 -> {
                             var permissionResult0 = ContextCompat.checkSelfPermission(requireContext(), CAMERA_PERMISSION[0])
                             Log.d("Signup_Image_Upload_Dialog", "checkSelfPermission$which")
@@ -72,73 +82,48 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
 
                         }
 
-                        // 카메라 2
+                        // 사진 1 -> 갤러리에 저장 함 (용량 큰). 이미지의 용량이 너무 크면 서버와 송수신할 때 데이터를 너무 많이 써서 용량 줄이는 작업 ㄱ,  사진 보정 가능
                         1 -> {
-                            var permissionResult1 = ContextCompat.checkSelfPermission(requireContext(), STORAGE_PERMISSION[0])
-                            Log.d("Signup_Image_Upload_Dialog", "checkSelfPermission$which")
 
-                            when (permissionResult1) {
-                                PackageManager.PERMISSION_GRANTED -> {
-                                    // 카메라, 저장소 권한이 이미 허용된 상태일 때
-                                    Log.d("Signup_Image_Upload_Dialog", "PERMISSION_GRANTED$which")
-                                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-                                    // 촬영한 사진이 저장될 파일 이름
-                                    val file_name = "/temp_${System.currentTimeMillis()}.jpg"
-                                    // 경로 + 파일 이름
-                                    val pic_path = "$file_path/$file_name"
-                                    val file = File(pic_path)
-
-                                    // 사진이 저장될 위치를 관리하는 Uri 객체
-                                    // val contentUri = Uri(pic_path) // 예전에는 파일명을 기술하면 바로 접근 가능
-                                    // -> 현재 안드로이드 OS 6.0 부터는 OS에서 해당 경로를 집어 넣으면 이 경로로 접근할 수 있는지 없는지를 판단. 접근할 수 있으면 Uri 객체를 넘겨줌.
-                                    contentUri = FileProvider.getUriForFile(requireContext(), "com.duos.camera.file_provider", file)
-
-                                    intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
-                                    startActivityForResult(intent, 200)
-
+                            // 거부 Or 아직 수락하지 않은 퍼미션을 저장할 String 배열리스트
+                            val rejectedPermissionList = ArrayList<String>()
+                            // 필요한 퍼미션들이 현재 권한을 받았는지 체크
+                            for (permission in requiredPermissions) {
+                                if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                                    //만약 권한이 없다면 rejectedPermissionList에 추가
+                                    rejectedPermissionList.add(permission)
                                 }
+                            }
 
-                                PackageManager.PERMISSION_DENIED -> {
-                                    Log.d("Signup_Image_Upload_Dialog", "PERMISSION_DENIED$which")
-                                    // 카메라, 저장소 권한이 거부된 상태일 때
-                                    requestPermissions(STORAGE_PERMISSION, STORAGE_PERMISSION_REQUEST)
-                                    // which가 0일 때와 똑같이 onRequestPermissionsResults 메서드 호출
-//                                    ActivityCompat.requestPermissions(requireActivity(), STORAGE_PERMISSION, STORAGE_PERMISSION_REQUEST)
-                                    // 이렇게 작성하면 허용 누르고 바로 카메라가 떠야 되는데 아닌가....
-//                                    permissionResult1 = ContextCompat.checkSelfPermission(requireContext(), STORAGE_PERMISSION[0])
-//                                    if (permissionResult1 == PackageManager.PERMISSION_GRANTED) {
-//                                        Log.d("Signup", "permissionResult 가 넘어갔나?")
-//                                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//                                        // 촬영한 사진이 저장될 파일 이름
-//                                        val file_name = "/temp_${System.currentTimeMillis()}.jpg"
-//                                        // 경로 + 파일 이름
-//                                        val pic_path = "$file_path/$file_name"
-//
-//                                        val file = File(pic_path)
-//
-//                                        // 사진이 저장될 위치를 관리하는 Uri 객체
-//                                        // val contentUri = Uri(pic_path) // 예전에는 파일명을 기술하면 바로 접근 가능
-//                                        // -> 현재 안드로이드 OS 6.0 부터는 OS에서 해당 경로를 집어 넣으면 이 경로로 접근할 수 있는지 없는지를 판단. 접근할 수 있으면 Uri 객체를 넘겨줌.
-//                                        contentUri = FileProvider.getUriForFile(requireContext(), "com.duos.camera.file_provider", file)
-//
-//                                        intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
-//                                        startActivityForResult(intent, 200)
-//                                    }
-                                }
+                            if (rejectedPermissionList.isNotEmpty()) {   // 거절된 퍼미션 있다면?
+                                val array = arrayOfNulls<String>(rejectedPermissionList.size)
+                                requestPermissions(rejectedPermissionList.toArray(array), multiplePermissionsCode)
+                            } else {// 모두 허용된 퍼미션이라면
+                                Log.d("Signup_Image_Upload_Dialog", "PERMISSION_GRANTED$which")
+                                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                                // 촬영한 사진이 저장될 파일 이름
+                                val file_name = "/temp_${System.currentTimeMillis()}.jpg"
+                                // 경로 + 파일 이름
+                                val pic_path = "$file_path/$file_name"
+                                val file = File(pic_path)
+
+                                // 사진이 저장될 위치를 관리하는 Uri 객체
+                                // val contentUri = Uri(pic_path) // 예전에는 파일명을 기술하면 바로 접근 가능
+                                // -> 현재 안드로이드 OS 6.0 부터는 OS에서 해당 경로를 집어 넣으면 이 경로로 접근할 수 있는지 없는지를 판단. 접근할 수 있으면 Uri 객체를 넘겨줌.
+                                contentUri = FileProvider.getUriForFile(requireContext(), "com.duos.camera.file_provider", file)
+
+                                intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
+                                startActivityForResult(intent, multiplePermissionsCode)
                             }
                         }
                         // 파일 접근
                         2 -> Log.d("Signup_Image_Upload_Dialog", "파일 접근 $which")
-
                     }
-
                 })
             dialogBuilder.create().show()
 
 
-            // 사진 1 ->갤러리에 저장 안함 (용량 작음)
-            // 사진 2 -> 갤러리에 저장 함 (용량 큰)
             // 갤러리에서 선택
 
 
@@ -164,27 +149,37 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
 
                 }
             }
-            STORAGE_PERMISSION_REQUEST -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 허용을 눌렀을 때 바로 카메라(1번 방식)로
-                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
-                    // 촬영한 사진이 저장될 파일 이름
-                    val file_name = "/temp_${System.currentTimeMillis()}.jpg"
-                    // 경로 + 파일 이름
-                    val pic_path = "$file_path/$file_name"
-                    val file = File(pic_path)
+            multiplePermissionsCode -> {
+                var startCam = true
+                if (grantResults.isNotEmpty()) {
+                    for ((i, permission) in permissions.withIndex()) {
+                        // 권한이 없는 permission이 있다면
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            Log.d("Signup", "사용하려면 권한 체크 해야되")
+                            Toast.makeText(requireContext(), "프로필 사진을 업로드하려면 카메라 접근 권한을 허용해야 합니다.", Toast.LENGTH_LONG).show()
+                            startCam = false
+                        }
+                    }
+                    if (startCam) {
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
-                    // 사진이 저장될 위치를 관리하는 Uri 객체
-                    // val contentUri = Uri(pic_path) // 예전에는 파일명을 기술하면 바로 접근 가능
-                    // -> 현재 안드로이드 OS 6.0 부터는 OS에서 해당 경로를 집어 넣으면 이 경로로 접근할 수 있는지 없는지를 판단. 접근할 수 있으면 Uri 객체를 넘겨줌.
-                    contentUri = FileProvider.getUriForFile(requireContext(), "com.duos.camera.file_provider", file)
+                        // 촬영한 사진이 저장될 파일 이름
+                        val file_name = "/temp_${System.currentTimeMillis()}.jpg"
+                        // 경로 + 파일 이름
+                        val pic_path = "$file_path/$file_name"
+                        val file = File(pic_path)
 
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
-                    startActivityForResult(intent, 200)
-                } else {
-                    Log.d("Signup_Image_Upload_Dialog", "OnRequestPermissionsResult에서 카메라1 권한 거부.")
-                    Toast.makeText(requireContext(), "프로필 사진을 업로드하려면 카메라 접근 권한을 허용해야 합니다.", Toast.LENGTH_LONG).show()
+                        // 사진이 저장될 위치를 관리하는 Uri 객체
+                        // val contentUri = Uri(pic_path) // 예전에는 파일명을 기술하면 바로 접근 가능
+                        // -> 현재 안드로이드 OS 6.0 부터는 OS에서 해당 경로를 집어 넣으면 이 경로로 접근할 수 있는지 없는지를 판단. 접근할 수 있으면 Uri 객체를 넘겨줌.
+                        contentUri = FileProvider.getUriForFile(requireContext(), "com.duos.camera.file_provider", file)
+
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
+                        startActivityForResult(intent, 200)
+                    }
+
+
                 }
             }
         }
@@ -197,24 +192,78 @@ class SignUpFragment05() : BaseFragment<FragmentSignup05Binding>(FragmentSignup0
         super.onActivityResult(requestCode, resultCode, data)
 
         when (requestCode) {
-            100 -> {
+            CAMERA_PERMISSION_REQUEST -> {
                 if (resultCode == RESULT_OK) {
                     // data : Intent 안에 사진 정보가 들어감
                     val bitmap = data?.getParcelableExtra<Bitmap>("data")
                     binding.signup05ProfileImageBackgroundIv.setImageBitmap(bitmap)
+                    binding.signup05ProfileImageBackgroundIv.scaleType = ImageView.ScaleType.FIT_XY
                 }
             }
-            200 -> {
+
+            multiplePermissionsCode -> {
                 if (resultCode == RESULT_OK) {
                     val bitmap = BitmapFactory.decodeFile(contentUri.path)
-                    binding.signup05ProfileImageBackgroundIv.setImageBitmap(bitmap)
+                    // 사진 조정 된것
+                    val degree = getDegree(contentUri, contentUri.path!!)   // contentUri 는 안드로이드 10버전 이상, contentUri.path!! 는 9버전 이하를 위해 넣음
+                    val bitmap2 = resizeBitmap(1024, bitmap)
+                    val bitmap3 = rotateBitmap(bitmap2, degree)
+                    
+                    binding.signup05ProfileImageBackgroundIv.setImageBitmap(bitmap3)
 
-//                     사진 파일 삭제한다.
+
+//                    // 사진 파일 삭제한다.
 //                    val file = File(contentUri.path)
 //                    file.delete()
+
                 }
             }
         }
+
     }
+
+    // 사진의 사이즈를 조정하는 메서드
+    fun resizeBitmap(targetWidth: Int, source: Bitmap): Bitmap {
+        // 이미지 비율 계산
+        val ratio = targetWidth.toDouble() / source.width.toDouble()
+        // 보정될 세로 길이 구하기
+        var targetHeight = (source.height * ratio).toInt()
+        // 크기를 조정한 bitmap 객체를 생성
+        val result = Bitmap.createScaledBitmap(source, targetWidth, targetHeight, false)
+        return result
+
+    }
+    // 이미지의 회전 각도값을 구하기
+//    11버전 이상부터 달라짐 (외부저장소 보안 때문에)
+    fun getDegree(uri:Uri, source: String):Float{
+        var exif:ExifInterface? = null
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            val photoUri = MediaStore.setRequireOriginal(uri)
+            val stream = requireActivity().contentResolver.openInputStream(photoUri)
+            exif = ExifInterface(source)
+        }else{
+            exif = ExifInterface(source)
+        }
+        var degree = 0
+        var ori = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1)   // 만약 회전값이 저장이 안되어 있으면 default값으로 -1 넣기 (0 넣으면 안댐)
+        when(ori){
+            ExifInterface.ORIENTATION_ROTATE_90 -> degree = 90
+            ExifInterface.ORIENTATION_ROTATE_180 -> degree = 180
+            ExifInterface.ORIENTATION_ROTATE_270 -> degree = 270
+        }
+        return degree.toFloat()
+    }
+    fun rotateBitmap(bitmap:Bitmap, degree : Float) : Bitmap{
+
+        // 각도값을 관리하는 객체
+        val matrix = Matrix()
+        matrix.postRotate(degree)
+        // 회전된 이미지를 받아온다.
+        val bitmap2 = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        return bitmap2
+    }
+
+
 
 }
