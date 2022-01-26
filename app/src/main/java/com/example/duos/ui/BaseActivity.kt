4021548bacecfad459d7d28330ad15aff0acd3a8
hjyk0310 <kjyk0835@naver.com>
2022-01-26 15:42:52 +0900

    temporary commit

--- app/src/main/java/com/example/duos/ui/BaseActivity.kt
+++ app/src/main/java/com/example/duos/ui/BaseActivity.kt
@@ -17,7 +17,7 @@ abstract class BaseActivity<T: ViewBinding>(private val inflate: (LayoutInflater
     protected lateinit var binding: T
         private set
 
-    private var imm : InputMethodManager? = null
+//    private var imm : InputMethodManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +26,14 @@ abstract class BaseActivity<T: ViewBinding>(private val inflate: (LayoutInflater
 
         AndroidThreeTen.init(this)
 
-        imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
+//        imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
 
-//        initKeyboard()
+        //initKeyboard()
 
         initAfterBinding()
     }
 
     protected abstract fun initAfterBinding()
-
 
     fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -51,42 +50,42 @@ abstract class BaseActivity<T: ViewBinding>(private val inflate: (LayoutInflater
         startActivity(intent)
     }
 
-    // 키보드 숨기기
-    fun hideKeyboard(v: View){
-        imm?.hideSoftInputFromWindow(v.windowToken, 0)
-    }
-
-    // 키보드 위에 버튼,뷰 보일 수 있게 하기
-    fun initKeyboard(){
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        binding.root.setOnApplyWindowInsetsListener { _, windowInsets ->
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                val imeHeight = windowInsets.getInsets(WindowInsets.Type.ime()).bottom
-                binding.root.setPadding(0, 0, 0, imeHeight)
-            }
-            else{
-                window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
-            }
-            windowInsets
-        }
-    }
-
-
-    // 키보드 바깥을 누르면 키보드를 숨김
-    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-        val focusView: View? = currentFocus
-        if (focusView != null) {
-            val rect = Rect()
-            focusView.getGlobalVisibleRect(rect)
-            val x = ev.x.toInt()
-            val y = ev.y.toInt()
-            if (!rect.contains(x, y)) {
-                val imm: InputMethodManager =
-                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(focusView.windowToken, 0)
-                focusView.clearFocus()
-            }
-        }
-        return super.dispatchTouchEvent(ev)
-    }
+//    // 키보드 숨기기
+//    fun hideKeyboard(v: View){
+//        imm?.hideSoftInputFromWindow(v.windowToken, 0)
+//    }
+//
+//    // 키보드 위에 버튼,뷰 보일 수 있게 하기
+//    fun initKeyboard(){
+//        WindowCompat.setDecorFitsSystemWindows(window, false)
+//        binding.root.setOnApplyWindowInsetsListener { _, windowInsets ->
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//                val imeHeight = windowInsets.getInsets(WindowInsets.Type.ime()).bottom
+//                binding.root.setPadding(0, 0, 0, imeHeight)
+//            }
+//            else{
+//                window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+//            }
+//            windowInsets
+//        }
+//    }
+//
+//
+//    // 키보드 바깥을 누르면 키보드를 숨김
+//    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+//        val focusView: View? = currentFocus
+//        if (focusView != null) {
+//            val rect = Rect()
+//            focusView.getGlobalVisibleRect(rect)
+//            val x = ev.x.toInt()
+//            val y = ev.y.toInt()
+//            if (!rect.contains(x, y)) {
+//                val imm: InputMethodManager =
+//                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+//                imm.hideSoftInputFromWindow(focusView.windowToken, 0)
+//                focusView.clearFocus()
+//            }
+//        }
+//        return super.dispatchTouchEvent(ev)
+//    }
 }
--- app/src/main/java/com/example/duos/ui/main/chat/ChattingActivity.kt
+++ app/src/main/java/com/example/duos/ui/main/chat/ChattingActivity.kt
@@ -21,12 +21,13 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.duos.R
 import com.example.duos.data.entities.MessageData
+import com.example.duos.ui.BaseActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-class ChattingActivity: AppCompatActivity() {
-    lateinit var binding: ActivityChattingBinding
+class ChattingActivity: BaseActivity<ActivityChattingBinding>(ActivityChattingBinding::inflate) {
+    //lateinit var binding: ActivityChattingBinding
     var roomIdx: Int = 0
     var userId: String = "tennis01"
     var partnerId: String = "djeikd0620"
@@ -35,73 +36,73 @@ class ChattingActivity: AppCompatActivity() {
     lateinit var chattingRV: RecyclerView
     lateinit var chattingEt: EditText
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityChattingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        
-        chattingEt = binding.chattingEt
-        chattingRV = binding.chattingMessagesRv
-
-        var sendBtn: ImageView = binding.chattingSendBtn
-
-        chattingRV.setHasFixedSize(true)
-        layoutManager = LinearLayoutManager(this)
-        (layoutManager as LinearLayoutManager).setStackFromEnd(true)    //
-        chattingRV.setLayoutManager(layoutManager)
-        chattingRV.setItemAnimator(DefaultItemAnimator())
-        chattingMessagesRVAdapter = ChattingMessagesRVAdapter()
-        chattingRV.setAdapter(chattingMessagesRVAdapter)
-
-        // chatting test code
-        var chattingMessage = MessageData("DATE", userId, roomIdx.toString(),"2022년 01월 21일", System.currentTimeMillis())
-        addChat(chattingMessage)
-        chattingRV.scrollToPosition(chattingMessagesRVAdapter.itemCount - 1)
-
-        chattingMessage = MessageData("MESSAGE", partnerId, userId,"상대방이 보낸 메세지입니다.", System.currentTimeMillis())
-        addChat(chattingMessage)
-        chattingRV.scrollToPosition(chattingMessagesRVAdapter.itemCount - 1)
-
-        
-        // 채팅 EditText focus되면 전송 아이콘(비행기 아이콘) primary색으로 활성화, 아닐때 비활성화
-        chattingEt.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if(chattingEt.length() > 0){
-                    sendBtn.isClickable = true
-                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_blue)
-                } else{
-                    sendBtn.isClickable = false
-                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_gray)
-                }
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-        })
-
-//        chattingEt.setOnFocusChangeListener(object : View.OnFocusChangeListener {
-//            override fun onFocusChange(view: View, hasFocus: Boolean) {
-//                if (hasFocus || !chattingEt.text.equals("")) {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        //binding = ActivityChattingBinding.inflate(layoutInflater)
+//        //setContentView(binding.root)
+//
+//        chattingEt = binding.chattingEt
+//        chattingRV = binding.chattingMessagesRv
+//
+//        var sendBtn: ImageView = binding.chattingSendBtn
+//
+//        chattingRV.setHasFixedSize(true)
+//        layoutManager = LinearLayoutManager(this)
+//        (layoutManager as LinearLayoutManager).setStackFromEnd(true)    //
+//        chattingRV.setLayoutManager(layoutManager)
+//        chattingRV.setItemAnimator(DefaultItemAnimator())
+//        chattingMessagesRVAdapter = ChattingMessagesRVAdapter()
+//        chattingRV.setAdapter(chattingMessagesRVAdapter)
+//
+//        // chatting test code
+//        var chattingMessage = MessageData("DATE", userId, roomIdx.toString(),"2022년 01월 21일", System.currentTimeMillis())
+//        addChat(chattingMessage)
+//        chattingRV.scrollToPosition(chattingMessagesRVAdapter.itemCount - 1)
+//
+//        chattingMessage = MessageData("MESSAGE", partnerId, userId,"상대방이 보낸 메세지입니다.", System.currentTimeMillis())
+//        addChat(chattingMessage)
+//        chattingRV.scrollToPosition(chattingMessagesRVAdapter.itemCount - 1)
+//
+//
+//        // 채팅 EditText focus되면 전송 아이콘(비행기 아이콘) primary색으로 활성화, 아닐때 비활성화
+//        chattingEt.addTextChangedListener(object: TextWatcher {
+//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//
+//            }
+//
+//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                if(chattingEt.length() > 0){
+//                    sendBtn.isClickable = true
 //                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_blue)
-//                } else {
+//                } else{
+//                    sendBtn.isClickable = false
 //                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_gray)
 //                }
 //            }
+//
+//            override fun afterTextChanged(p0: Editable?) {
+//
+//            }
 //        })
-        sendBtn.setOnClickListener{
-                v -> sendMessage(v)
-        }
-
-        binding.chattingMakePlanBtn.setOnClickListener ({
-            val intent = Intent(this, MakePlanActivity::class.java)
-            startActivity(intent)
-        })
-    }
+//
+////        chattingEt.setOnFocusChangeListener(object : View.OnFocusChangeListener {
+////            override fun onFocusChange(view: View, hasFocus: Boolean) {
+////                if (hasFocus || !chattingEt.text.equals("")) {
+////                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_blue)
+////                } else {
+////                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_gray)
+////                }
+////            }
+////        })
+//        sendBtn.setOnClickListener{
+//                v -> sendMessage(v)
+//        }
+//
+//        binding.chattingMakePlanBtn.setOnClickListener ({
+//            val intent = Intent(this, MakePlanActivity::class.java)
+//            startActivity(intent)
+//        })
+//    }
 
     // 리사이클러뷰에 채팅 추가
     private fun addChat(data: MessageData) {
@@ -154,5 +155,69 @@ class ChattingActivity: AppCompatActivity() {
     // System.currentTimeMillis를 몇시:몇분 am/pm 형태의 문자열로 반환
     private fun toDate(currentMiliis: Long): String {
         return SimpleDateFormat("a hh:mm").format(Date(currentMiliis))
+    }
+
+    override fun initAfterBinding() {
+        chattingEt = binding.chattingEt
+        chattingRV = binding.chattingMessagesRv
+
+        var sendBtn: ImageView = binding.chattingSendBtn
+
+        chattingRV.setHasFixedSize(true)
+        layoutManager = LinearLayoutManager(this)
+        (layoutManager as LinearLayoutManager).setStackFromEnd(true)    //
+        chattingRV.setLayoutManager(layoutManager)
+        chattingRV.setItemAnimator(DefaultItemAnimator())
+        chattingMessagesRVAdapter = ChattingMessagesRVAdapter()
+        chattingRV.setAdapter(chattingMessagesRVAdapter)
+
+        // chatting test code
+        var chattingMessage = MessageData("DATE", userId, roomIdx.toString(),"2022년 01월 21일", System.currentTimeMillis())
+        addChat(chattingMessage)
+        chattingRV.scrollToPosition(chattingMessagesRVAdapter.itemCount - 1)
+
+        chattingMessage = MessageData("MESSAGE", partnerId, userId,"상대방이 보낸 메세지입니다.", System.currentTimeMillis())
+        addChat(chattingMessage)
+        chattingRV.scrollToPosition(chattingMessagesRVAdapter.itemCount - 1)
+
+
+        // 채팅 EditText focus되면 전송 아이콘(비행기 아이콘) primary색으로 활성화, 아닐때 비활성화
+        chattingEt.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(chattingEt.length() > 0){
+                    sendBtn.isClickable = true
+                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_blue)
+                } else{
+                    sendBtn.isClickable = false
+                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_gray)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+
+//        chattingEt.setOnFocusChangeListener(object : View.OnFocusChangeListener {
+//            override fun onFocusChange(view: View, hasFocus: Boolean) {
+//                if (hasFocus || !chattingEt.text.equals("")) {
+//                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_blue)
+//                } else {
+//                    sendBtn.setImageResource(R.drawable.ic_btn_airplane_send_gray)
+//                }
+//            }
+//        })
+        sendBtn.setOnClickListener{
+                v -> sendMessage(v)
+        }
+
+        binding.chattingMakePlanBtn.setOnClickListener ({
+            val intent = Intent(this, MakePlanActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
