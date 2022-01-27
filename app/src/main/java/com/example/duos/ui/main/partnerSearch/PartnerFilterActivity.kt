e9e96daf05984c738f18f3daf5fb01796a98b623
hjyk0310 <kjyk0835@naver.com>
2022-01-28 05:44:03 +0900

    customRangeSlider custom과 필터 화면 기능 구현 완료 (지역설정, 성별 선택 제외)

--- app/src/main/java/com/example/duos/ui/main/partnerSearch/PartnerFilterActivity.kt
+++ app/src/main/java/com/example/duos/ui/main/partnerSearch/PartnerFilterActivity.kt
@@ -1,20 +1,32 @@
 package com.example.duos.ui.main.partnerSearch
 
+import android.content.Intent
+import android.view.View
 import com.example.duos.databinding.ActivityPartnerFilterBinding
 import com.example.duos.ui.BaseActivity
+import com.example.duos.ui.main.MainActivity
 import com.jaygoo.widget.OnRangeChangedListener
 import com.jaygoo.widget.RangeSeekBar
 
 class PartnerFilterActivity: BaseActivity<ActivityPartnerFilterBinding>(ActivityPartnerFilterBinding::inflate) {
     override fun initAfterBinding() {
 
-        val rangeSeekbar = binding.partnerFilterAgeRangeSb
-        rangeSeekbar.steps = 10
+        val ageRangeSeekbar = binding.partnerFilterAgeRangeSb
+        val ballCapacityRangeSeekBar = binding.partnerFilterBallCapabilityRangeSb
 
-        rangeSeekbar.setOnRangeChangedListener(object: OnRangeChangedListener {
+        ageRangeSeekbar.setProgress(20f,60f)
+        ballCapacityRangeSeekBar.setProgress(0f, 10f)
+
+        ageRangeSeekbar.setOnRangeChangedListener(object: OnRangeChangedListener {
             override fun onRangeChanged(view: RangeSeekBar?, leftValue: Float, rightValue: Float, isFromUser: Boolean) {
-                binding.partnerFilterAgeMinTv.text = leftValue.toInt().toString()
-                binding.partnerFilterAgeMaxTv.text = rightValue.toInt().toString()
+                if(leftValue.toInt() % 10 == 0){
+                    binding.partnerFilterAgeMinTv.text = leftValue.toInt().toString()
+                }
+                if(rightValue.toInt() % 10 == 0){
+                    binding.partnerFilterAgeMaxTv.text = rightValue.toInt().toString()
+                }
+//                changeSeekBarIndicator(rangeSeekbar.leftSeekBar, leftValue)
+//                changeSeekBarIndicator(rangeSeekbar.rightSeekBar, rightValue)
             }
 
             override fun onStartTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
@@ -25,5 +37,68 @@ class PartnerFilterActivity: BaseActivity<ActivityPartnerFilterBinding>(Activity
 
             }
         })
+
+        ballCapacityRangeSeekBar.setOnRangeChangedListener(object: OnRangeChangedListener{
+            override fun onRangeChanged(view: RangeSeekBar?, leftValue: Float, rightValue: Float, isFromUser: Boolean) {
+
+                if(leftValue.toInt() % 1 == 0){
+                    var leftUnit = "년"
+                    if(leftValue < 1f){     // == 0f로 하면 0과 1 사이에서 0개월이 아닌 0년으로 뜨는 구간이 생김
+                        leftUnit = "개월"
+                    }
+                    binding.partnerFilterBallCapabilityMinTv.text = leftValue.toInt().toString() + leftUnit
+                }
+
+                if(rightValue.toInt() % 1 == 0){
+                    var rightUnit = "년"
+                    if(rightValue < 1f){
+                        rightUnit = "개월"
+                    }
+                    binding.partnerFilterBallCapabilityMaxTv.text = rightValue.toInt().toString() + rightUnit
+                }
+            }
+
+            override fun onStartTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
+
+            }
+
+            override fun onStopTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
+
+            }
+        })
+
+        binding.partnerFilterApplyTv.setOnClickListener ({
+            var location = "지역 받아와~"
+            var gender = "성별 받아와~"
+            var ageMin = binding.partnerFilterAgeMinTv.text
+            var ageMax = binding.partnerFilterAgeMaxTv.text
+            var ballCapacityMin = binding.partnerFilterBallCapabilityMinTv.text
+            var ballCapacityMax = binding.partnerFilterBallCapabilityMaxTv.text
+
+            // 이 위치에서 파트너 찾기 GET을 하고, onSuccess에서 그 결과를 로컬에 저장하고
+            // PartnerSearchFragment에서는 계속 GET해오는 것이 아니라 로컬에 저장되어있던 정보들을 load 해줘야 할 듯 (인터넷이 연결되어있지 않을 때도 메인화면이 보일 수 있게)
+
+            // 파트너 찾기 - 매칭 화면으로 이동
+            //val intent = Intent(this, MakePlanActivity::class.java)
+            startActivity(Intent(this, MainActivity::class.java))
+        })
     }
+
+// indicator(10의 단위 위에 위치할때마다 위에 표시해줌)
+//    private fun changeSeekBarIndicator(seekbar: SeekBar, value: Float){
+//        seekbar.showIndicator(true)
+//        if (Utils.compareFloat(value, 20f, 4) == 0){
+//            seekbar.setIndicatorText("20대")
+//        }else if (Utils.compareFloat(value, 30f, 4) == 0){
+//            seekbar.setIndicatorText("30대")
+//        }else if (Utils.compareFloat(value, 40f, 4) == 0){
+//            seekbar.setIndicatorText("40대")
+//        }else if (Utils.compareFloat(value, 50f, 4) == 0){
+//            seekbar.setIndicatorText("50대")
+//        }else if (Utils.compareFloat(value, 60f, 4) == 0){
+//            seekbar.setIndicatorText("60대")
+//        }else {
+//            seekbar.showIndicator(false)
+//        }
+//    }
 }
