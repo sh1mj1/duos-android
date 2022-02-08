d43ababad4ea2c03c22438fda13ba6a63e6a4e1a
sh1mj1 <7wlgns@gmail.com>
2022-02-08 22:27:30 +0900

    지난 약속, 리뷰 작성API DONE , 마이페이지,프로필 ROOMDB DONE

--- app/src/main/java/com/example/duos/utils/NetworkModule.kt
+++ app/src/main/java/com/example/duos/utils/NetworkModule.kt
@@ -1,23 +1,12 @@
 package com.example.duos.utils
 
-import android.content.Context
 import android.util.Log
 import com.example.duos.ApplicationClass.Companion.BASE_URL
-import com.example.duos.ApplicationClass.Companion.mSharedPreferences
 import com.example.duos.config.XAccessTokenInterceptor
-import com.example.duos.data.remote.accessToken.AccessTokenResultData
 import com.example.duos.data.remote.accessToken.AccessTokenService
-import com.google.android.gms.common.api.Api
-import com.google.android.gms.common.internal.service.Common
-import com.google.gson.GsonBuilder
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import okhttp3.*
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import java.util.concurrent.TimeUnit
 
 
