6a7b0fc41d5c23996f217d511d974268242f0485
junho <junholim98@gmail.com>
2022-02-10 04:17:50 +0900

    Appointment ExistView에서 chatListitem import 제거

--- app/src/main/java/com/example/duos/ui/main/chat/appointment/AppointmentExistView.kt
+++ app/src/main/java/com/example/duos/ui/main/chat/appointment/AppointmentExistView.kt
@@ -1,6 +1,6 @@
 package com.example.duos.ui.main.chat.appointment
 
-import com.example.duos.data.entities.ChatListItem
+
 
 interface AppointmentExistView {
     fun onAppointmentExistSuccess(appointmentIdx : Int)
