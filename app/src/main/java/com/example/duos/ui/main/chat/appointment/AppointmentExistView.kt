32c9b267001c51636d3704fbe80f2a4b9be186c0
junho <junholim98@gmail.com>
2022-02-10 03:53:49 +0900

    다크모드 Theme 라이트모드와 동일하게 변경

--- app/src/main/java/com/example/duos/ui/main/chat/appointment/AppointmentExistView.kt
+++ app/src/main/java/com/example/duos/ui/main/chat/appointment/AppointmentExistView.kt
@@ -1,5 +1,7 @@
 package com.example.duos.ui.main.chat.appointment
 
+import com.example.duos.data.entities.ChatListItem
+
 interface AppointmentExistView {
     fun onAppointmentExistSuccess(appointmentIdx : Int)
     fun onAppointmentExistFailure(code: Int, message: String)
