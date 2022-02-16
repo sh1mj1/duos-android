068f0184dde67fe0ced9985c1dd9fb1dae90c5b9
hjyk0310 <kjyk0835@naver.com>
2022-02-17 00:29:03 +0900

    약속잡기 api의 result로 appointmentIdx를 받아 룸디비에도 반영하도록 수정, 약속 시간은 1시간 후부터~ 토스트메세지 띄우도록 수정

--- app/src/main/java/com/example/duos/ui/main/appointment/MakeAppointmentView.kt
+++ app/src/main/java/com/example/duos/ui/main/appointment/MakeAppointmentView.kt
@@ -1,6 +1,8 @@
 package com.example.duos.ui.main.appointment
 
+import com.example.duos.data.remote.appointment.MakeAppointmentResult
+
 interface MakeAppointmentView {
-    fun onMakeAppointmentSuccess()
+    fun onMakeAppointmentSuccess(result: MakeAppointmentResult)
     fun onMakeAppointmentFailure(code: Int, message: String)
 }
