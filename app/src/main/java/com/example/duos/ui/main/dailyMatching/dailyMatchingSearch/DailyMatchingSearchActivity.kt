dcca2103088d99ff51be505fc409d96e388694b5
심지훈 <simjihun@simjihuns-MacBook-Air.local>
2023-04-18 23:10:46 +0900

    fix: 데일리매칭 검색 쪽 해결 중(2) 검색어를 눌러서 검색을 했을 때 이상이 발생

--- app/src/main/java/com/example/duos/ui/main/dailyMatching/dailyMatchingSearch/DailyMatchingSearchActivity.kt
+++ app/src/main/java/com/example/duos/ui/main/dailyMatching/dailyMatchingSearch/DailyMatchingSearchActivity.kt
@@ -8,6 +8,8 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
 import com.example.duos.data.entities.dailyMatching.SearchHistory
 import com.example.duos.data.entities.dailyMatching.SearchHistoryDatabase
 import com.example.duos.data.remote.dailyMatching.DailyMatchingListService
@@ -32,16 +34,23 @@ class DailyMatchingSearchActivity :
     private var dailyMatchingSearchListDatas = ArrayList<SearchResultItem>()
     private var isNewSearch = true
 
+    lateinit var historyRv: RecyclerView
+
+    lateinit var db: SearchHistoryDatabase
+
     @SuppressLint("NotifyDataSetChanged")
     override fun initAfterBinding() {
+        db = SearchHistoryDatabase.getInstance(this)!!
         val searchEt = binding.dailyMatchingSearchEt
         val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        historyRv = binding.dailyMatchingSearchRecordRv
+
         initView()
 
         searchEditTextListener(imm, searchEt)
 
         // 검색 기록 전체 삭제
-        deleteAllHistory()
+        deleteAllHistoryListener()
 
         binding.dailyMatchingSearchBackIv.setOnClickListener {
             finish()
@@ -70,12 +79,11 @@ class DailyMatchingSearchActivity :
         }
     }
 
-    private fun deleteAllHistory() {
+    private fun deleteAllHistoryListener() {
         binding.dailyMatchingDeleteSearchRecordTv.setOnClickListener {
-            val db = SearchHistoryDatabase.getInstance(this)
-            db!!.searchHistoryRoomDao().clearAll()
+            db.searchHistoryRoomDao().clearAll()
             Log.e(TAG, "전체 삭제 후 getAll ${db.searchHistoryRoomDao().getAll()}")
-            binding.dailyMatchingSearchRecordRv.visibility = View.GONE
+            historyRv.visibility = View.GONE
             binding.dailyMatchingSearchRecordNullTv.visibility = View.VISIBLE
             binding.dailyMatchingSearchResultCountTv.visibility = View.GONE
             binding.dailyMatchingSearchRecentCl.visibility = View.VISIBLE
@@ -85,8 +93,7 @@ class DailyMatchingSearchActivity :
 
     // 검색 후 키워드 Room DB에 저장
     private fun saveSearchKeyword(keyword: String) {
-        val db = SearchHistoryDatabase.getInstance(this)
-        val searchedKeywordList = db!!.searchHistoryRoomDao().searchKeyword(keyword)
+        val searchedKeywordList = db.searchHistoryRoomDao().searchKeyword(keyword)
 
         Log.e(TAG, "searchedKeywordList: $searchedKeywordList")
         searchedKeywordList.listIterator().forEach {
@@ -101,10 +108,11 @@ class DailyMatchingSearchActivity :
         if (isNewSearch) {
             db.searchHistoryRoomDao().insert(SearchHistory(null, keyword))
         }
-        val keywords = db.searchHistoryRoomDao().getAll().reversed()
+
+        val keywords = db.searchHistoryRoomDao().getAll()
         Log.e(TAG, "showResultOfSearch - $keywords")
         searchHistoryAdapter.submitList(keywords)
-        binding.dailyMatchingSearchRecordRv.visibility = View.VISIBLE
+        historyRv.visibility = View.VISIBLE
         binding.dailyMatchingSearchResultCountTv.visibility = View.VISIBLE
         binding.dailyMatchingResultOfSearchFl.visibility = View.VISIBLE
     }
@@ -112,8 +120,6 @@ class DailyMatchingSearchActivity :
     private fun search(keyword: String) {
         // 검색한 단어를 RoomDB 에 넣기
         saveSearchKeyword(keyword)
-
-//        showResultOfSearch()
 
         // 그리고 보이면 안되는 것들 invisible 로 만들기
         binding.dailyMatchingSearchResultCountTv.visibility = View.VISIBLE // 검색결과 갯수
@@ -126,17 +132,15 @@ class DailyMatchingSearchActivity :
 
     // 최근 검색어 삭제
     private fun deleteSearchKeyword(keyword: String) {
-        val db = SearchHistoryDatabase.getInstance(this)
-        db!!.searchHistoryRoomDao().delete(keyword)
+        db.searchHistoryRoomDao().delete(keyword)
         showResultOfSearch()
     }
 
     private fun showResultOfSearch() {
-        val db = SearchHistoryDatabase.getInstance(this)
-        val keywords = db!!.searchHistoryRoomDao().getAll().reversed()
+        val keywords = db.searchHistoryRoomDao().getAll()
         Log.e(TAG, "showResultOfSearch - $keywords")
         searchHistoryAdapter.submitList(keywords)
-        binding.dailyMatchingSearchRecordRv.visibility = View.VISIBLE
+        historyRv.visibility = View.VISIBLE
         binding.dailyMatchingSearchResultCountTv.visibility = View.VISIBLE
         binding.dailyMatchingResultOfSearchFl.visibility = View.VISIBLE
     }
@@ -148,6 +152,7 @@ class DailyMatchingSearchActivity :
         Log.d(TAG, "searchHistoryItem clicked - 선택한 아이템으로 검색")
         binding.dailyMatchingSearchEt.setText(keyword)
         search(keyword)
+
     }
 
 
@@ -157,9 +162,16 @@ class DailyMatchingSearchActivity :
             historyItemClickListener = { searchHistoryItem(it) }
         )
 
-        binding.dailyMatchingSearchRecordRv.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        binding.dailyMatchingSearchRecordRv.adapter = searchHistoryAdapter
+        searchHistoryAdapter.registerAdapterDataObserver(object : AdapterDataObserver() {
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                historyRv.layoutManager!!.smoothScrollToPosition(historyRv, null, searchHistoryAdapter.itemCount)
+            }
+        })
+        historyRv.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true)
+
+        historyRv.adapter = searchHistoryAdapter
+
     }
 
     private fun initView() {
@@ -169,17 +181,16 @@ class DailyMatchingSearchActivity :
         binding.dailyMatchingSearchRecentCl.visibility = View.VISIBLE
 
         // 처음 검색하는 것이면 (DB 가 비어있으면) 처음에는 최근 검색어가 없습니다.
-        val db = SearchHistoryDatabase.getInstance(this)
-        val mySearchHistory = db!!.searchHistoryRoomDao().getAll()
+        val mySearchHistory = db.searchHistoryRoomDao().getAll()
         Log.d(TAG, "검색어 DB 가 비어있나 $mySearchHistory , isEmpty? : ${mySearchHistory.isEmpty()}")
 
         initHistoryRV()
 
         if (mySearchHistory.isEmpty()) {    // 처음 검색
-            binding.dailyMatchingSearchRecordRv.visibility = View.GONE
+            historyRv.visibility = View.GONE
             binding.dailyMatchingSearchRecordNullTv.visibility = View.VISIBLE
         } else {    // 검색한 적 있음
-            binding.dailyMatchingSearchRecordRv.visibility = View.VISIBLE
+            historyRv.visibility = View.VISIBLE
             binding.dailyMatchingSearchRecordNullTv.visibility = View.GONE
             showResultOfSearch()
         }
@@ -192,7 +203,7 @@ class DailyMatchingSearchActivity :
         val allDailyMatchingSearchRV = DailyMatchingSearchRVAdapter(dailyMatchingSearchListDatas)
         val recyclerView = binding.allDailyMatchingRecyclerviewRc
         recyclerView.adapter = allDailyMatchingSearchRV
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
         binding.dailyMatchingSearchResultCountTv.text = "검색 결과 (${dailyMatchingSearchResultData.resultSize})"
         binding.dailyMatchingResultOfSearchFl.visibility = View.VISIBLE
@@ -204,6 +215,7 @@ class DailyMatchingSearchActivity :
         binding.allDailyMatchingRecyclerviewRc.visibility = View.VISIBLE
         binding.dailyMatchingSearchResultCountTv.visibility = View.VISIBLE // 검색결과 갯수
         binding.dailyMatchingSearchRecentCl.visibility = View.GONE
+
 
         progressOFF()
 
